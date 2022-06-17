848ad080a4b48168e0e54b8a8cf9155ec1a19797
Dmitry Badichan <d.badichan@youngcapital.nl>
2022-06-17 20:25:25 +0300

    add rspec to model advert

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :user do
-    email { 'dima@mail.ru' }
+    sequence(:email) { |n| "pserson#{n}@example.com" }
     name  { 'Dima' }
     admin { false }
     password { 'dima123' }
     password_confirmation { 'dima123' }
+
   end
 end
