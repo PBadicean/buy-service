26f289a521e79b864a2281792d45755fe1d6382a
Dmitry Badichan <d.badichan@youngcapital.nl>
2022-06-17 18:26:44 +0300

    add gem factory_bot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
+require 'simplecov'
+SimpleCov.start 'rails'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'associations' do
+    it { should have_many(:adverts).class_name('Advert') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe "#client?" do
+    context "user a client" do
+      let(:user) { create :user }
+      it "returns true" do
+        expect(user.admin?).to eq false
+      end
+    end
+
+    context "user is not client" do
+      let(:user) { create :user,
+                  name: "Admin",
+                  email: "admin@gmail.com",
+                  admin: true }
+      it "returns false" do
+        expect(user.admin?).to eq true
+      end
+    end
+  end
+
 end
