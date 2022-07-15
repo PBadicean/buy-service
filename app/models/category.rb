3b492092cc6b2262cf757c44a0e3601f4e334732
Dmitry Badichan <d.badichan@youngcapital.nl>
2022-07-15 12:54:30 +0300

    spec model category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  has_many :adversts
+  has_many :adverts
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Request, type: :model do
   describe 'associations' do
     it { should belong_to(:user).class_name('User') }
     it { should belong_to(:advert).class_name('Advert') }
+    it { should belong_to(:category).class_name('Category') }
   end
 
 end
